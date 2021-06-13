b0e72ce867a6ce01cc10d449b41eb0b2872c6eb5
eleven702 <85826516+eleven702@users.noreply.github.com>
2021-06-13 23:00:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -458,7 +458,7 @@ function generate_certificate() {
 function configure_web() {
   rm -rf /www/xray_web
   mkdir -p /www/xray_web
-  wget -O web.tar.gz https://github.com/eleven702/web/blob/main/web.tar.gz
+  wget -O web.tar.gz https://raw.githubusercontent.com/eleven702/yb702/main/basic/web.tar.gz
   tar xzf web.tar.gz -C /www/xray_web
   judge "站点伪装"
   rm -f web.tar.gz
